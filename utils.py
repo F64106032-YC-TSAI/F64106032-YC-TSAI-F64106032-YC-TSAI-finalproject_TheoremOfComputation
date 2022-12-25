0257f0d813bcd0b7305f7839d5b4d1b94762a9de
F64106032-YC-TSAI <121308153+F64106032-YC-TSAI@users.noreply.github.com>
2022-12-25 22:49:59 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, StickerSendMessage
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -12,6 +12,27 @@ def send_text_message(reply_token, text):
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
     return "OK"
+
+def send_image_url(reply_token, img_url):
+    line_bot_api = LineBotApi(channel_access_token)
+    message = ImageSendMessage(
+        original_content_url=img_url,
+        preview_image_url=img_url
+    )
+    line_bot_api.reply_message(reply_token, message)
+    return "OK"
+
+#def send_stricker(reply_token, num1, num2):
+#    line_bot_api = LineBotApi(channel_access_token)
+#    message = StickerSendMessage(
+#        packge_id=num1,
+#        stricker_id=num2
+#    )
+#    line_bot_api.reply_message(reply_token, message)
+#    return "OK"
+
+
+
 
 
 """
